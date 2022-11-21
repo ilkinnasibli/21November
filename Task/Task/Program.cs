b396d71f7dcd608314b51dbbf12463813de6cf4a
ilkinnasibli <ilkinnasibli@mail.ru>
2022-11-21 23:58:57 +0400

    task

---
+++ Task/Task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+#region Append
+//stringbuilder builder = new stringbuilder();
+//builder.append("alma");
+//builder.append("heyva");
+//console.writeline(builder.tostring());
+
+
+
+#endregion
+
+//string append(string word1, string word2)
+//{
+//    string appendtext = " ";
+//    console.write(appendtext = word1 + word2);
+//    return appendtext;
+//}
+//append("5", "heyva");
+#region Remove
+//StringBuilder stringBuilder = new StringBuilder("Hello World!");
+//stringBuilder.Remove(0,6);
+//Console.WriteLine(stringBuilder);
+#endregion
+#region Replace
+StringBuilder myStringBuilder = new StringBuilder("Hello World!");
+myStringBuilder.Replace('!', '?');
+Console.WriteLine(myStringBuilder);
+#endregion
+
